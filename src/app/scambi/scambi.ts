bcd444f65868653d9daecd2ac26c686935d919b2
SamuD03 <167511300+SamuD03@users.noreply.github.com>
2025-06-11 21:39:45 +0200

    secondo commit

--- src/app/scambi/scambi.ts
+++ src/app/scambi/scambi.ts
@@ -1,27 +1,47 @@
 import { Component } from '@angular/core';
-import {DUMMYCARDS} from '../../DUMMYCARDS';
+
 import {FormsModule} from '@angular/forms';
+import {MatIcon} from '@angular/material/icon';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {MatFormField, MatInputModule, MatLabel} from '@angular/material/input';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {DUMMYCARDS} from '../../DUMMYCARDS';
+import {Button} from '../shared/button/button';
+
+enum State {
+  scambi,
+  proposte,
+  crea
+}
 
 @Component({
   selector: 'app-scambi',
   imports: [
-    FormsModule
+    FormsModule,
+    MatIcon,
+    MatTabGroup,
+    MatTab,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatLabel,
+    MatInputModule,
+    Button
   ],
   templateUrl: './scambi.html',
   styleUrl: './scambi.css'
 })
 export class Scambi {
-dummyCards: any[] = DUMMYCARDS;
-offeredCards: any[] = DUMMYCARDS;
-selectedRequestedCard: any = null;
-selectedOfferedCard: any = null;
+  status: State = State.scambi;
+  protected readonly State = State;
+  dummyCards : any[] = DUMMYCARDS;
+  selectedRequest: string = '';
+  selectedOffer: string = '';
+  message: string = '';
 
-  updateOfferedCards() {
-    if(this.selectedRequestedCard) {
-      console.log(this.selectedRequestedCard);
-      this.offeredCards = this.dummyCards.filter(card => card.id !== this.selectedRequestedCard.id);
-    } else {
-      this.offeredCards = [];
-    }
+  onSubmit() {
+  console.log(this.selectedRequest);
+  console.log(this.selectedOffer)
+    console.log(this.message);
   }
 }
